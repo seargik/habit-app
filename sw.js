c852d9fd7bb145368fda5adb780e357efa8a77fe
seargik <sergej_arefjev@yahoo.com>
2026-08-12 23:40:19 +0300

    Cache legacy carryover compatibility layer

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE = "life-tracker-v4-phase1b";
+const CACHE = "life-tracker-v4-phase1c";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=4",
   "./migration-v4.js?v=4",
+  "./legacy-carryover-v4.js?v=4",
   "./general-guard-v4.js?v=4",
   "./app-v4.js?v=4",
   "./manifest.webmanifest",
